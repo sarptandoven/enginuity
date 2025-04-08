40031d6dba1086a16d26c64ae68add65ce3985f5
sarptandoven <sarptan.doven1@gmail.com>
2025-04-08 16:31:52 +0300

    Github actions deploy fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/Engiunity' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Engiunity/' : '',
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
