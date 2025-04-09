8acbb562321835a650faa1464fd64866bc48cbc3
sarptandoven <sarptan.doven1@gmail.com>
2025-04-09 20:55:11 +0300

    github pages build error fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    domains: ['randomuser.me']
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/Engiunity' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Engiunity/' : '',
+  trailingSlash: true,
   env: {
     // Add your environment variables here
   },
