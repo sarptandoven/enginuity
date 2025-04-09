57ce00c5327a1400bce901a61a6d67670f7b02cf
sarptandoven <sarptan.doven1@gmail.com>
2025-04-09 21:19:46 +0300

    github pages build error trial fix 2

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/Engiunity' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Engiunity/' : '',
   trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
